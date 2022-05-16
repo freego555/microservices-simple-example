097e4d482bd7739fbd990fedb53023d189c0b2da
Ihor Rohovets <freego555@gmail.com>
2022-05-16 18:26:21 +0300

    S02 - L22 Handling CORS Errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
