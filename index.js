2e7757e6942c167c1c49a96c4295253ab50b201f
Ihor Rohovets <freego555@gmail.com>
2022-05-18 13:44:04 +0300

    S02 - L33 Emitting Events

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,13 +14,22 @@ app.get('/posts', (req, res) => {
  res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = {
     id, title
   };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id, title
+    }
+  }).catch((err) => {
+    console.error(err.message);
+  });
 
   res.status(201).send(posts[id]);
 });
